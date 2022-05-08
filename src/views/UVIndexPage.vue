c48b1e491049b323dc02a8296d1ba18c856d6d61
Ken Sodemann <ken@ionic.io>
2022-05-08 07:01:57 -0500

    feat(uv): implement the UV index page

--- src/views/UVIndexPage.vue
+++ src/views/UVIndexPage.vue
@@ -5,19 +5,43 @@
         <ion-title>UV Index</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="main-content" :fullscreen="true">
-      <ExploreContainer name="UV Index page" />
+    <ion-content class="main-content ion-text-center ion-padding" :fullscreen="true">
+      <csdemo-uv-index class="primary-value" :uvIndex="currentWeather?.uvIndex"></csdemo-uv-index>
+      <div class="description">{{ advice }}</div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { CsdemoUvIndex } from '@ionic-enterprise/cs-demo-weather-widgets-vue';
+import useWeather from '@/use/weather';
 
 export default defineComponent({
   name: 'UVIndexPage',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  components: { CsdemoUvIndex, IonHeader, IonToolbar, IonTitle, IonContent, IonPage },
+  setup() {
+    const { currentWeather, getUVAdvice } = useWeather();
+    const advice = computed(() => {
+      if (currentWeather.value) {
+        return getUVAdvice(currentWeather.value.uvIndex);
+      } else {
+        return '';
+      }
+    });
+
+    return { currentWeather, advice };
+  },
 });
 </script>
+
+<style scoped>
+.description {
+  margin-top: 1em;
+}
+
+.main-content {
+  --padding-top: 5em;
+}
+</style>
